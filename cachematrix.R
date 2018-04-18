fdd4cceb38b173695dc021e999864d880668a61a
Emmnouil <38495929+Emmnouil@users.noreply.github.com>
2018-04-18 15:27:41 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# provides a list of fucntions to: set a defined matrix, get/print the defined matrix, 
+## setsolve to define the of the solve function, getsolve to print the result of the solved matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solv <- NULL
+    set <- function(y) {
+        x <<- y
+        solv <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(Solve) solv <<- Solve
+    getSolve <- function() solv
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+# recalls the result of the solve function from the memory if it has already been
+## calculated (or set by use of the makeCacheMatrix function), 
+### otherwise it executes the solve function 
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    solv <- x$getSolve()
+    if(!is.null(solv)) {
+        message("getting cached data")
+        return(solv)
+    }
+    data <- x$get()
+    solv <- solve(data, ...)
+    x$setSolve(solv)
+    solv
 }
